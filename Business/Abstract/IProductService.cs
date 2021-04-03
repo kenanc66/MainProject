92a7e2347f86088790f0f347c6d6266e15aea8b7
kenanc66 <kenancicekdev@gmail.com>
2021-04-03 21:55:32 +0300

    CoreAndDTO-Added

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Entities.Concrete;
+using Entities.DTOs;
+
 namespace Business.Abstract
 {
     public interface IProductService
@@ -9,6 +11,6 @@ namespace Business.Abstract
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitPrice(decimal min,decimal max);
-
+        List<ProductDetailDto> GetProductDetails();
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,18 +9,29 @@ namespace ConsoleUI
     {//open closed principle S-O-LID
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EFProductDal());
-            foreach (var product in productManager.GetAllByCategoryId(2))
-            {
-                Console.WriteLine(product.ProductName);
-            }
-            Console.WriteLine("-----------------------");
-            foreach (var product in productManager.GetByUnitPrice(2,1000))
-            {
-                Console.WriteLine(product.ProductName);
-            }
+             ProductTest();
+           // CategoryTest();
+        }
 
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new
+                            CategoryManager(new EFCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
 
+        private static void ProductTest()
+        {
+            ProductManager productManager = new
+                            ProductManager(new EFProductDal());
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName+"/" +product.CategoryName);
+            }
+            
         }
     }
 }
