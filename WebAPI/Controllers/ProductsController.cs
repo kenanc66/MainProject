a6d8557b09308d4435e24ae18f9917eb0d50cb28
kenanc66 <kenancicekdev@gmail.com>
2021-04-19 18:21:05 +0300

    Final Version

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -56,6 +56,17 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+        [HttpGet("getproductdetails")]
+        public IActionResult GetProductDetails(int categoryId)
+        {
+            var result = _productService.GetProductDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest(result);
+        }
         [HttpPost("add")]
         //delete and update can use httppost
         public IActionResult Add(Product product)
